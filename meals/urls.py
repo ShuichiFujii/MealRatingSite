6131e84860ff1d7c3479e10df011066a9c0fd5f9
ShuichiFujii <22ec101@ms.dendai.ac.jp>
2026-05-04 02:14:15 +0900

    urls.pyの追加と変更

---
+++ meals/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.landing_page, name="landing"),
+    path("category/<str:category>/", views.category_list, name="category_list"),
+    path("meal/<int:meal_id>/", views.meal_detail, name="meal_detail"),
+]
